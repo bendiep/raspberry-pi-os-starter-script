b935144379dd2a7bbd870fd36ea4fdf0c65f00ae
Ben Diep <ben_diep@hotmail.com>
2022-07-29 18:04:38 +1000

    update ssh-tunnelling.sh

--- ssh-tunnelling.sh
+++ ssh-tunnelling.sh
@@ -3,16 +3,17 @@
 # Install ngrok (ssh tunnelling)
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
 unzip ngrok-stable-linux-arm.zip
-./ngrok --version
+sudo cp ngrok /usr/local/bin
+ngrok --version
 
 # Set auth token (get from website)
-# ./ngrok authtoken [YOUR AUTH TOKEN]
+# ngrok authtoken [YOUR AUTH TOKEN]
 
 # Start Ngrok server (public access)
-# ./ngrok tcp 22
+# ngrok tcp 22 > /dev/null &
 
 # Stop Ngrok server (local access)
-# Ctrl + C
+# kill [pid]
 
 # Note:
 #  - TTL - how many login attempts
