f524e2d1d0db4bce4a7ada6516b306b52b0f844c
Ben Diep <ben_diep@hotmail.com>
2022-07-30 15:46:11 +1000

    add python, php and config to script

--- starter.sh
+++ starter.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 
+# Install Git
+sudo apt update -y
+sudo apt install git -y
+
+# Install Nodejs
+sudo apt install nodejs -y
+
+# Install NPM
+sudo apt install npm -y
+
+# Install Python
+sudo apt install python -y
+
+# Install PHP
+sudo apt install php -y
+
+# Install ruby
+sudo apt install ruby -y
+
+# Install Ack
+sudo apt install ack -y
+
 # Update ssh welcome message
 echo "Welcome ${LOGNAME} to this private linux server." >> .profile
 
 # Setup aliases
 echo "alias login-attempt-accepted=\"cat /var/log/auth.log | grep 'Accepted password'\"" >> .profile
 echo "alias login-attempt-failed=\"cat /var/log/auth.log | grep 'Failed password'\"" >> .profile
+echo "alias python=python3" >> .profile
 
-# Install git
-sudo apt update -y
-sudo apt install git -y
-
-# Install nodejs
-sudo apt install nodejs -y
-
-# Install npm
-sudo apt install npm -y
-
-# Install ruby
-sudo apt install ruby -y
-
-# Install ack
-sudo apt install ack -y
+# History search settings
+echo "bind '\"\e[A\": history-search-backward'" >> .profile
+echo "bind '\"\e[B\": history-search-forward'" >> .profile
 
 # Display about of disk space left on device
 df -h
